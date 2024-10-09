a7c6fe715af5484037ee6f4387a6bc35bd07e28b
Hardik Chavda <chavdahardik1729@gmail.com>
2024-10-09 19:57:00 +0530

    Add files via upload

---
+++ Areaofcircle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float r;
+    printf("Enter Redius :-");
+    scanf("%f",&r);
+    float pi = 3.14;
+    float Area = pi*r*r;
+    printf("Area of Circle is :-%f",Area);
+    
+
+}
